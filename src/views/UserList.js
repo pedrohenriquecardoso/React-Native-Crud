80cdfb8dc62ea7e8f93fdc8929153e393c5004fa
pedrohenriquecardoso <pedrohcardoso.1995@gmail.com>
2021-11-14 19:48:04 -0300

    listItem react native elements

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,9 +1,29 @@
 import React from 'react'
-import {Text, StyleSheet} from 'react-native'
+import {Text, StyleSheet, FlatList, View} from 'react-native'
+import { ListItem, Avatar } from 'react-native-elements'
+import users from '../data/users'
 
 export default props => {
+    function getUserItem({item: user}){
+        return (
+            <ListItem bottomDivider onPress={()=> props.navigation.navigate('UserForm')}>
+                <Avatar source={{uri: user.avatarUrl}} />
+                <View>
+                    <ListItem.Title>{user.name}</ListItem.Title>
+                    <ListItem.Subtitle>{user.email}</ListItem.Subtitle> 
+                </View>
+            </ListItem>
+        )
+    }
+
     return (
-        <Text style={styles.text}>UserList</Text>
+        <View>
+            <FlatList
+                keyExtractor={user => user.id.toString()}
+                data={users}
+                renderItem={getUserItem}
+            />
+        </View>
     )
 }
 
