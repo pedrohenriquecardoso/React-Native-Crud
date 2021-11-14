188a9b97fc9303967c34d048ff6fd47fbb3a1fe3
pedrohenriquecardoso <pedrohcardoso.1995@gmail.com>
2021-11-14 19:47:49 -0300

    users list

---
+++ src/data/users.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        id: 1,
+        name: 'Joao Silva',
+        email: 'JoaoSilva@jaosilva.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2014/04/03/10/32/businessman-310819_960_720.png'
+    },
+    {
+        id: 2,
+        name: 'Maria Aguiar',
+        email: 'mariaAguiar@mariaAguiar.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2014/04/03/10/32/user-310807_960_720.png'
+    },
+    {
+        id: 3,
+        name: 'Antonio Joao',
+        email: 'Antonio@email.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/04/01/11/25/avatar-1300331_960_720.png'
+    },
+    {
+        id: 4,
+        name: 'Julio Fraga',
+        email: 'fraga@email.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_960_720.png'
+    },
+]
