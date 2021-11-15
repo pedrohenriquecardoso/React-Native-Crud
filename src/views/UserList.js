3717d0c81417ba1063f690130c6ec810e6a357bb
pedrohenriquecardoso <pedrohcardoso.1995@gmail.com>
2021-11-15 00:51:25 -0300

    list item swipeable buttons edit/delete

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,18 +1,36 @@
+import { getActionFromState } from '@react-navigation/core'
 import React from 'react'
 import {Text, StyleSheet, FlatList, View} from 'react-native'
-import { ListItem, Avatar } from 'react-native-elements'
+import { ListItem, Avatar, Button, Icon } from 'react-native-elements'
 import users from '../data/users'
 
 export default props => {
     function getUserItem({item: user}){
         return (
-            <ListItem bottomDivider onPress={()=> props.navigation.navigate('UserForm')}>
+            <ListItem.Swipeable 
+                leftContent={
+                    <Button
+                        onPress={()=> props.navigation.navigate('UserForm')}
+                        title="edit"
+                        icon={{ name: 'edit', color: 'orange' }}
+                        buttonStyle={{ minHeight: '100%' }}
+                    />
+                }
+                rightContent={
+                    <Button
+                        title="Delete"
+                        icon={{ name: 'delete', color: 'white' }}
+                        buttonStyle={{ minHeight: '100%', backgroundColor: 'red' }}
+                    />
+                }
+                bottomDivider 
+            >
                 <Avatar source={{uri: user.avatarUrl}} />
                 <View>
                     <ListItem.Title>{user.name}</ListItem.Title>
                     <ListItem.Subtitle>{user.email}</ListItem.Subtitle> 
                 </View>
-            </ListItem>
+            </ListItem.Swipeable>
         )
     }
 
@@ -31,4 +49,12 @@ const styles = StyleSheet.create({
     text: {
       color: 'black',
     },
+    rowFlexStart: {
+        flexDirection: "row",
+        justifyContent: 'space-between'
+    },
+    rowFlexEnd:{
+        flexDirection: "row",
+        justifyContent: 'flex-end'
+    }
 });
