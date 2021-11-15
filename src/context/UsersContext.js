5dedd446646529f6ef751adccb7cbfc840e8544e
pedrohenriquecardoso <pedrohcardoso.1995@gmail.com>
2021-11-15 13:43:19 -0300

    create user function

--- src/context/UsersContext.js
+++ src/context/UsersContext.js
@@ -5,9 +5,18 @@ const initialState = {users}
 const UsersContext = createContext({})
 
 const actions = {
+    createUser(state, action){
+        const user = action.payload
+        user.id = Math.random()
+        return {
+           ...state,
+           users: [...state.users, user] 
+        }
+    },
     deleteUser(state, action){
         const user = action.payload
         return {
+            ...state,
             users: state.users.filter(u => u.id !== user.id)
         }
     }
